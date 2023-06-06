8faa1eacd31c4cca1907db0e8c8c5eecb9d7a24a
Faris Chew <farischew@gmail.com>
2023-06-06 18:58:05 +0800

    Adds useUser hook for sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,3 +1,9 @@
+import useUser from "../hooks/use-user";
+
 export default function Sidebar() {
+  const {
+    user: { fullName, userId, username },
+  } = useUser();
+
   return <div>sidebar</div>;
 }
