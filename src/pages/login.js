932ee8743194d39874f01cacf023cd0216e3da2c
Faris Chew <farischew@gmail.com>
2023-06-06 15:58:33 +0800

    Adds and sets up tailwin config with postcss

--- src/pages/login.js
+++ src/pages/login.js
@@ -14,5 +14,14 @@ export default function Login() {
     document.title = "Login - Reactagram";
   }, []);
 
-  return <h1>Hello</h1>;
+  return (
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+      <div className="flex w-3/5">
+        <img src="/images/iphone-with-profile.jpg" alt="iPhone with Profile" />
+      </div>
+      <div className="flex flex-col w-2/5">
+        <p>This will be the form!</p>
+      </div>
+    </div>
+  );
 }
