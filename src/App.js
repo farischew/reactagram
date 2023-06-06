e94b76f47f7cfdb10c938075bdc265a9082068cb
Faris Chew <farischew@gmail.com>
2023-06-06 17:18:02 +0800

    Adds Not Found Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
+import NotFound from "./pages/not-found";
 
 const Login = lazy(() => import("./pages/login"));
 const Signup = lazy(() => import("./pages/signup"));
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path={ROUTES.LOGIN} element={<Login />} />
           <Route path={ROUTES.SIGN_UP} element={<Signup />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Suspense>
     </Router>
