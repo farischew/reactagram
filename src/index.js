e587874a5b43aedae59b7865b8bb6b9f5db6a266
Faris Chew <farischew@gmail.com>
2023-06-06 14:59:03 +0800

    Adds firebase init and context with seed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import FirebaseContext from "./context/firebase";
+import { firebase, FieldValue } from "./lib/firebase";
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <FirebaseContext.Provider value={{ firebase, FieldValue }}>
+    <App />
+  </FirebaseContext.Provider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/lib/firebase.js
@@ -0,0 +1,19 @@
+import Firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+// import { seedDatabase } from '../seed';
+
+const config = {
+  apiKey: "AIzaSyCTQVvqpB5xeBtqkRetoNkJlXvE9rKCQ2g",
+  authDomain: "reactagram-de198.firebaseapp.com",
+  projectId: "reactagram-de198",
+  storageBucket: "reactagram-de198.appspot.com",
+  messagingSenderId: "481852562848",
+  appId: "1:481852562848:web:c6498285b4ae59800958c5",
+};
+
+const firebase = Firebase.initializeApp(config);
+const { FieldValue } = Firebase.firestore;
+
+export { firebase, FieldValue };
