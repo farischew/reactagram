7fddd117804315a85115c1e9714b36a79421561a
Faris Chew <farischew@gmail.com>
2023-06-06 18:24:09 +0800

    Adds Header component wit functional buttons

--- src/App.js
+++ src/App.js
@@ -2,21 +2,29 @@ import { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
 import NotFound from "./pages/not-found";
+import useAuthListener from "./hooks/use-auth-listener";
+import UserContext from "./context/user";
 
 const Login = lazy(() => import("./pages/login"));
 const Signup = lazy(() => import("./pages/signup"));
+const Dashboard = lazy(() => import("./pages/dashboard"));
 
 function App() {
+  const { user } = useAuthListener();
+
   return (
-    <Router>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Routes>
-          <Route path={ROUTES.LOGIN} element={<Login />} />
-          <Route path={ROUTES.SIGN_UP} element={<Signup />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Suspense>
-    </Router>
+    <UserContext.Provider value={{ user }}>
+      <Router>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Routes>
+            <Route path={ROUTES.LOGIN} element={<Login />} />
+            <Route path={ROUTES.SIGN_UP} element={<Signup />} />
+            <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
