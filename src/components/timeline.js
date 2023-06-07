d9844d1839f6b7c8ab30c2581740cb62171bf08b
Faris Chew <farischew@gmail.com>
2023-06-07 14:23:47 +0800

    Adds Header and Image to Post

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,3 +1,25 @@
+import Skeleton from "react-loading-skeleton";
+import usePhotos from "../hooks/use-photos";
+import Post from "./post";
+
 export default function Timeline() {
-  return <div className="caontainer col-span-2">timeline</div>;
+  const { photos } = usePhotos();
+
+  console.log(photos);
+
+  return (
+    <div className="caontainer col-span-2">
+      {!photos ? (
+        <>
+          <Skeleton count={1} width={640} height={400} className="mb-5" />
+        </>
+      ) : photos?.length > 0 ? (
+        photos.map((content) => (
+          <Post key={content.docId} content={content}></Post>
+        ))
+      ) : (
+        <p className="text-center text-2xl">Follow people to see posts!</p>
+      )}
+    </div>
+  );
 }
