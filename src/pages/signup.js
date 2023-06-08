1020ca24be6f418fc89fba6b38e830ea2e258199
Faris Chew <farischew@gmail.com>
2023-06-08 14:55:36 +0800

    Adds followers empty array on signup

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -42,6 +42,7 @@ export default function Login() {
           fullName,
           emailAddress: emailAddress.toLowerCase(),
           following: [],
+          followers: [],
           dateCreated: Date.now(),
         });
 
