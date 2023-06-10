7b04768d79504a90fd453b2fcce758d72754c9ba
Faris Chew <farischew@gmail.com>
2023-06-10 21:46:09 +0800

    Bugfixes

--- src/components/createpost/index.js
+++ src/components/createpost/index.js
@@ -5,7 +5,7 @@ export default function CreatePost(props) {
 
   return (
     <>
-      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+      <div className="justify-center items-center bg-color-slate max-h-full w-full flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
         <div className="relative w-auto my-6 mx-auto max-w-3xl">
           {/*content*/}
           <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
@@ -41,11 +41,11 @@ export default function CreatePost(props) {
                 Close
               </button>
               <button
-                className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                className="bg-emerald-500 active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                 type="button"
                 onClick={handleModalClose}
               >
-                Save Changes
+                Create Post
               </button>
             </div>
           </div>
