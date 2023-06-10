d27928df6368493845845df33445cfdef97b530a
Faris Chew <farischew@gmail.com>
2023-06-10 19:46:52 +0800

    Added menubar to dashboard and removed header

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import Header from "../components/header";
 import Timeline from "../components/timeline";
 import Sidebar from "../components/sidebar";
+import MenuBar from "../components/menubar";
 
 export default function Dashboard() {
   useEffect(() => {
@@ -10,8 +11,9 @@ export default function Dashboard() {
 
   return (
     <div className="bg-gray-background">
-      <Header />
-      <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
+      {/* <Header /> */}
+      <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg pt-4">
+        <MenuBar />
         <Timeline />
         <Sidebar />
       </div>
