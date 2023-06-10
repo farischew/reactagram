e2d7343f0acf4223213625eb8e9ef30213a6a0d5
Faris Chew <farischew@gmail.com>
2023-06-10 21:29:05 +0800

    Added a modal

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,21 +1,29 @@
-import { useEffect } from "react";
-import Header from "../components/header";
+import { useEffect, useState } from "react";
+// import Header from "../components/header";
 import Timeline from "../components/timeline";
 import Sidebar from "../components/sidebar";
 import MenuBar from "../components/menubar";
+import CreatePost from "../components/createpost";
 
 export default function Dashboard() {
+  const [showModal, setShowModal] = useState(false);
+
   useEffect(() => {
     document.title = "Reactagram";
   }, []);
+
+  const handleModalToggle = () => {
+    setShowModal(!showModal);
+  };
 
   return (
     <div className="bg-gray-background">
       {/* <Header /> */}
       <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg pt-4">
-        <MenuBar />
+        <MenuBar handleModalToggle={handleModalToggle} />
         <Timeline />
         <Sidebar />
+        {showModal && <CreatePost handleModalToggle={handleModalToggle} />}
       </div>
     </div>
   );
