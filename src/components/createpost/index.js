df01b08fda2c8c49b1004373c3b40f4fab0acb57
Faris Chew <farischew@gmail.com>
2023-06-12 16:19:44 +0800

    Reerted changes

--- src/components/createpost/index.js
+++ src/components/createpost/index.js
@@ -41,11 +41,10 @@ export default function CreatePost(props) {
 
   return (
     <>
-      <div className="justify-center items-center bg-slate opacity-70 max-h-full w-full flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"></div>
-      <div className="justify-center items-center max-h-full w-full flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-        <div className="relative w-1/2 h-4/5 my-6 mx-auto max-w-3xl">
+      <div className="justify-center items-center bg-color-slate max-h-full w-full flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+        <div className="relative w-auto my-6 mx-auto max-w-3xl">
           {/*content*/}
-          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full h-full bg-white outline-none focus:outline-none">
+          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
             {/*header*/}
             <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
               <h3 className="text-3xl font-semibold">Create a post</h3>
